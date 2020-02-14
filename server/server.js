2e812ae395243fdbbe0d5414042ab38f325aa9cc
ahmadkhaleel96 <ahmadkhaleel96@yahoo.com>
2020-02-14 15:11:42 +0200

    finished the tcp chat app

---
+++ server/server.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+/* eslint-disable no-redeclare */
+'use strict';
+
+const net = require('net');
+const PORT = 3001;
+const server = net.createServer();
+
+let socketPool = {};
+
+server.on('connection', (socket) => {
+  const id = `Socket-${Math.random()}`;
+  socketPool[id] = socket;
+  socket.on('data', (buffer) => dispatchEvent(buffer));
+  socket.on('end', () => delete socketPool[id]);
+  socket.on('error', (e) => console.error('Socket ERR:', e));
+});
+
+function dispatchEvent(buffer) {
+  let message = JSON.parse(buffer.toString().trim());
+  broadcast(message);
+}
+
+function broadcast(message) {
+  let payload = JSON.stringify(message);
+  for (let socket in socketPool) {
+    socketPool[socket].write(payload);
+  }
+}
+
+server.listen(PORT, () => {
+  console.log(`listening on the coolest port: ${PORT}`);
+});
